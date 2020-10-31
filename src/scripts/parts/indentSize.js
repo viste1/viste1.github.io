bf333c887f8998cf691fca0053734dfad0bd25f4
Vitaliy <viste012@gmail.com>
2020-10-31 21:53:56 +0300

    'initial'

--- src/scripts/parts/indentSize.js
+++ src/scripts/parts/indentSize.js
@@ -1,12 +1,10 @@
 export default function indentSize() {
     {
-        /*
         let check_height = 473;
         let img_height = $(".section-popular__js-img-height").height();
         alert(img_height);
         if (img_height <= check_height) {
-            $(".section-popular__second-row h3").css("margin-top", 10+"rem");
+            //$(".section-popular__second-row h3").css("margin-top", 10+"rem");
         }
-         */
     }
 }
--- src/scripts/parts/localStorage.js
+++ src/scripts/parts/localStorage.js
@@ -1,7 +1,5 @@
 export default function initStorage() {
-    /*
     let input_value = $(".section-writeUs__form_email").val()
     sessionStorage.setItem('storage', input_value);
     $(".section-writeUs__form_email").val(sessionStorage.getItem('storage'));
-     */
 }
