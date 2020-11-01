d1df4b657514395a1461e48a518a472c08794bde
Vitaliy <viste012@gmail.com>
2020-11-01 03:23:11 +0300

    'initial'

--- src/scripts/parts/difLazy.js
+++ src/scripts/parts/difLazy.js
@@ -1,7 +1,7 @@
 export default function initDifLazy() {
-        $('.js-difLazy').each((index, el) => {
-            const $image = $(el);
-            const realSrc = $image.attr('data-src');
-            $image.attr('src', realSrc);
-        });
+    $('.js-difLazy').each((index, el) => {
+        const $image = $(el);
+        const realSrc = $image.attr('data-src');
+        $image.attr('src', realSrc);
+    });
 }
