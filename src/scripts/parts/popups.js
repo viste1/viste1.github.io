ab776e3313ba0c6acb2a9666a58f3edcd448b54d
viste1 <viste012@gmail.com>
2020-11-18 13:41:54 +0300

    'added-item-form'

--- src/scripts/parts/popups.js
+++ src/scripts/parts/popups.js
@@ -2,4 +2,7 @@ export default function initPopups() {
     $('.section-footer__js-popup-rules').magnificPopup({
         type: 'inline'
     });
+    $('.item-info__grid-select-container__form-size-table__js-popup-sizes').magnificPopup({
+        type: 'inline'
+    });
 }
